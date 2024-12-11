64d653440356797e25919b3c554ca9a7991cda99
Yadhugovind C <yadhugovindc@gmail.com>
2024-12-11 19:47:21 +0530

    sixth commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import PlayerContextProvider from "./context/PlayerContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/spotify-clone">
     <PlayerContextProvider>
       <App />
     </PlayerContextProvider>
